7ada989d454f00ff24c00726494fb18632d91d61
Nabawi <nabawicode@gmail.com>
2026-07-04 23:46:16 +0700

    feat(routing/schemas): implement JWT authentication (still halfway through)

--- app/api/routers/v1_users.py
+++ app/api/routers/v1_users.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, status, Depends
-from sqlalchemy import select
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
@@ -10,15 +13,110 @@ from app.database import get_db
 from app.schemas import (
     UserCreate,
     UserUpdate,
-    UserResponse,
-    PostResponse
+    UserPrivate,
+    UserPublic,
+    PostResponse,
+    Token
     )
 
+from app.auth import create_access_token, hash_password, verify_access_token, oauth2_scheme, verify_password
+from app.config import settings
 
 router = APIRouter()
 
+@router.post(
+    "",
+    response_model=UserPrivate,
+    status_code=status.HTTP_201_CREATED)
+async def create_user(
+    user: UserCreate,
+    db: Annotated[AsyncSession, Depends(get_db)]):
+    result = await db.execute(select(models.User).where(
+            (models.User.username == user.username) 
+            | (models.User.username == user.username)
+            )
+        )
+    
+    exists = result.scalars().first()
+    if exists:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail= "User already exists",
+        )
+    
+    new_user = models.User(
+        username=user.username,
+        email=user.email.lower(),
+        password_hashed=hash_password(user.password)
+    )
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+    
+    return new_user
 
-@router.get("/{user_id}", response_model=UserResponse)
+@router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    result = await db.execute(
+        select(models.User)
+        .where(func.lower(models.User.email) == form_data.username.lower())
+        )
+    
+    user = result.scalars().first()
+    if not user or not verify_password(form_data.password, user.password_hashed):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authentication": "Bearer"}
+        )
+    
+    access_token_expires = timedelta(minutes=settings.access_token_expire_minutes)
+    access_token = create_access_token(
+        data={"sub": str(user.id)},
+        expires_delta=access_token_expires
+    )
+    
+    return Token(access_token=access_token, token_type="bearer")
+
+@router.get("/me", response_model=UserPrivate)
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: Annotated[AsyncSession, Depends(get_db)],
+):
+    user_id = verify_access_token(token)
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    try:
+        user_id_int = int(user_id)
+    except (TypeError, ValueError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    result = await db.execute(
+        select(models.User).where(models.User.id == user_id_int)
+    )
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+@router.get("/{user_id}", response_model=UserPublic)
 async def get_user(
     user_id: int,
     db: Annotated[AsyncSession, Depends(get_db)]):
@@ -65,36 +163,10 @@ async def get_user_posts(
     posts = result.scalars().all()
     return posts
 
-@router.post(
-    "",
-    response_model=UserResponse,
-    status_code=status.HTTP_201_CREATED)
-async def create_user(
-    user: UserCreate,
-    db: Annotated[AsyncSession, Depends(get_db)]):
-    result = await db.execute(select(models.User).where(
-            (models.User.username == user.username) 
-            | (models.User.username == user.username)
-            )
-        )
-    
-    exists = result.scalars().first()
-    if exists:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail= "User already exists",
-        )
-    
-    new_user = models.User(**user.model_dump())
-    db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
-    
-    return new_user
 
 @router.patch(
     "/{user_id}",
-    response_model=UserResponse,
+    response_model=UserPrivate,
     status_code=status.HTTP_200_OK)
 async def update_user_partial(
     user_id: int,
